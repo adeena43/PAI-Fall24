5217d7ce5fc55257b3ce2bd080cb09f6f69f4211
adeena43 <142868138+adeena43@users.noreply.github.com>
2024-08-28 19:37:54 +0500

    Create task03.py

---
+++ Lab02/task03.py
@@ -0,0 +1,8 @@
+def convert_url(url):
+    if url.startswith("http://www."):
+        return url.replace("http://www.", "", 1)
+    else:
+        return "Invalid URL format. Make sure it starts with 'http://www.'."
+user_url = input("Enter your URL starting with 'http://www.': ")
+converted_url = convert_url(user_url)
+print(f"Converted URL: {converted_url}")
