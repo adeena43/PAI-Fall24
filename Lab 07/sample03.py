613e1658228554f8c295278a11be3c071ef1e784
Adina Faraz <adinafraz01@gmail.com>
2024-10-02 09:45:28 +0500

    Create sample03.py

---
+++ Lab 07/sample03.py
@@ -0,0 +1,14 @@
+#filter is applied on
+#single group, single colum,n
+#single group, multiple column
+import pandas as pd
+
+df = pd.read_csv("heart (1).csv")
+df = df.rename(columns = {"sex":"Gender"})
+df["Gender"][df["Gender"]==0] = "female"
+df["Gender"][df["Gender"]==1] = "male"
+
+print(df.groupby('Gender').mean())
+
+print(df.groupby('chol').max())
+print(df)
