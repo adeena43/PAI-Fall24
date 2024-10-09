8df3d06e732ce05d7e1fac8b5613f55857ac1812
Adina Faraz <adinafraz01@gmail.com>
2024-10-09 09:12:22 +0500

    Update task01.py

--- Lab 08/task01.py
+++ Lab 08/task01.py
@@ -1,1 +1,7 @@
-###
+#1. Write a program to create numpy array or vector of all the even integers
+#  from 20 to 50 exluding 20 and 50.
+import numpy as np
+
+arr = np.arange(22, 50, 2)
+arr = np.array(arr)
+print(arr)
